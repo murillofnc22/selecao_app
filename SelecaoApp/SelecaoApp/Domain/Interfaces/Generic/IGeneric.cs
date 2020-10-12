5fbd3f2ab8b4076be9ff6557dab1e911266be7e3
Murillo <murillofnc@gmail.com>
2020-10-12 16:30:36 -0300

    Adicionando interfaces para tornar generica a comunicação com o BD

---
+++ SelecaoApp/SelecaoApp/Domain/Interfaces/Generic/IGeneric.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SelecaoApp.Domain.Interfaces.Generic
+{
+    public interface IGeneric<T> where T : class
+    {
+        void Add(T objeto);
+        void Update(T objeto);
+        void Delete(T objeto);
+        List<T> List();
+    }
+}
---
+++ SelecaoApp/SelecaoApp/Domain/Interfaces/IProduto.cs
@@ -0,0 +1,9 @@
+﻿using SelecaoApp.Domain.Interfaces.Generic;
+using SelecaoApp.Entities;
+
+namespace SelecaoApp.Domain.Interfaces
+{
+    public interface IProduto : IGeneric<Produtos>
+    {
+    }
+}
