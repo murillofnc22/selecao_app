9613706cbd21c9172c3d222533f4e9fda1a06a28
Murillo <murillofnc@gmail.com>
2020-10-12 15:12:08 -0300

    Primeiro commit - Criando a tela de login e implementando parte da lógica

---
+++ SelecaoApp/SelecaoApp/Login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelecaoApp
+{
+    public class Login
+    {
+        private string Usuario { get; set; }
+        private string Senha { get; set; }
+
+        public Login(string usuario, string senha)
+        {
+            Usuario = usuario;
+            Senha = senha;
+        }
+
+        public bool HandleLogin()
+        {
+            return true;
+        }
+    }
+}
---
+++ SelecaoApp/SelecaoApp/frmLogin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SelecaoApp
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Sair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            LogaNoSistema();
+        }
+
+        private void LogaNoSistema()
+        {
+            Login login = new Login(txtUsuario.Text, txtSenha.Text);
+            if (login.HandleLogin())
+            {
+                this.Hide();
+                frmPrincipal principal = new frmPrincipal();
+                principal.ShowDialog();
+            }
+            this.Close();
+        }
+    }
+}
